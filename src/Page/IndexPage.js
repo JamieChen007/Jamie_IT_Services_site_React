ce89e538c61c706eafeeab2ccd96355ae2690130
JamieChen007 <107536976+JamieChen007@users.noreply.github.com>
2023-10-30 01:00:37 +1100

    feat/add index page head

--- src/Page/IndexPage.js
+++ src/Page/IndexPage.js
@@ -6,8 +6,20 @@ import WorkWithReasons from "../components/WorkWithReasons/WorkWithReasons";
 import SocialMediaCounter from "../components/SocialMediaCounter";
 import ContactSlogan from "../components/ContactSlogan";
 import { INDEX_CONTENT } from "../constants/indexPage.Const";
+import { useEffect } from "react";
 
 const IndexPage = () => {
+  useEffect(() => {
+    document.title =
+      "Jamie IT Services - Home | Reliable IT Company - Free Quote";
+    document
+      .querySelector("meta[name='description']")
+      .setAttribute(
+        "content",
+        "Jamie IT Services offers high-quality IT Services in Sydney. Our IT company offers a wide range of IT services to assist your business."
+      );
+  }, []);
+
   return (
     <main className="mainContent">
       <PicContainer content={INDEX_CONTENT.picContainerContent} />
